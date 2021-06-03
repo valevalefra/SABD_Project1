d2de626157834368aa974cd43061c3e60c211055
michelablack <michelacamilli1996@gmail.com>
2021-06-03 10:20:35 +0200

    Comments added

--- src/main/java/query1/Query1_Preprocessing.java
+++ src/main/java/query1/Query1_Preprocessing.java
@@ -27,12 +27,12 @@ public class Query1_Preprocessing {
 
         JavaRDD<String> useful_csv_rows = dataset1.filter(row -> !(row.equals(firstLine1)));
 
-        JavaPairRDD<String, Integer> datasetSum = useful_csv_rows.mapToPair(line -> {
+        JavaPairRDD<String, Integer> rddSum = useful_csv_rows.mapToPair(line -> {
             String[] lineSplit = line.split(",");
             return new Tuple2<>((lineSplit[0]), 1);}).reduceByKey(Integer::sum);
 
         //joinDataset returns: country, date, complete name of country, total vaccinations
-        JavaPairRDD<String, Tuple2<Tuple3<String, String, String>, Integer>> joinDataset = dataset3
+        JavaPairRDD<String, Tuple2<Tuple3<String, String, String>, Integer>> joinRdd = dataset3
                 .filter(row -> (!(row.equals(firstLine2)) && row.split(",")[0].compareTo(START_DATE)>=0))
                 .mapToPair(line -> {
                     String[] lineSplit = line.split(",");
@@ -40,9 +40,9 @@ public class Query1_Preprocessing {
                     Tuple3<String, String, String> columns = new Tuple3<>(lineSplit[0],lineSplit[2],lineSplit[lineSplit.length-1]);
                     //putting as key the region id
                     return new Tuple2<>((lineSplit[1]), columns);
-                }).join(datasetSum);
+                }).join(rddSum);
 
-        return joinDataset.mapToPair(row -> {
+        return joinRdd.mapToPair(row -> {
             Tuple3<String, String, Integer> columns = new Tuple3<>(row._2._1._2(), row._2._1._3(), row._2._2());
             return new Tuple2<>(String.valueOf(row._2._1._1()), columns);
         }).sortByKey(true).cache();
